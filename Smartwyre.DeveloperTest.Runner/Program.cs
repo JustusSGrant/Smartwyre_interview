ab48ca537dfa034985063be865f8c6c27538f7b5
JustusSGrant <jgrant615@gmail.com>
2023-09-07 22:49:07 -0400

    additional project directories

---
+++ Smartwyre.DeveloperTest.Runner/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Smartwyre.DeveloperTest.Models;
+using Smartwyre.DeveloperTest.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Smartwyre.DeveloperTest.Data.Interfaces;
+using Smartwyre.DeveloperTest.Data;
+
+namespace Smartwyre.DeveloperTest.Runner;
+
+public static class Program
+{
+
+    static void Main(string[] args)
+    {
+        // setup DI
+        var serviceProvider = new ServiceCollection()
+            .AddScoped<IRebateService, RebateService>()
+            .AddSingleton<IProductDataStore, ProductDataStore>()
+            .AddSingleton<IRebateDataStore, RebateDataStore>()
+            .BuildServiceProvider();
+        CalculateRebate(serviceProvider);
+    }
+
+    private static void CalculateRebate(ServiceProvider serviceProvider)
+    {
+        var request = new CalculateRebateRequest()
+        {
+            RebateIdentifier = String.Empty,
+            ProductIdentifier = String.Empty,
+            Volume = 0m
+        };
+
+        var _rebateService = serviceProvider.GetService<IRebateService>();
+        Console.WriteLine("Welcome to Justus Grant's Interview app!");
+        Console.WriteLine("I am about to ask for information required to calculate your rebate!.");
+        Console.WriteLine("Please enter the Rebate Identifier: ");
+        request.RebateIdentifier = Console.ReadLine();
+        Console.WriteLine("Please enter your Product Identifier: ");
+        request.ProductIdentifier = Console.ReadLine();
+        Console.WriteLine("Lastly, please enter decimal value for Volume: ");
+        request.Volume = Decimal.Parse(Console.ReadLine());
+        Console.WriteLine("Thank you for provideng that information.");
+        var result = _rebateService.Calculate(request);
+        Console.WriteLine($"Success status for your rebate: {result.Success}") ;
+        Console.ReadLine();
+    }
+}
---
+++ Smartwyre.DeveloperTest.Tests/RebateService.Tests.cs
@@ -0,0 +1,286 @@
+using System;
+using Moq;
+using NUnit.Framework;
+using Smartwyre.DeveloperTest.Data.Interfaces;
+using Smartwyre.DeveloperTest.Services;
+using Smartwyre.DeveloperTest.Models;
+
+namespace Smartwyre.DeveloperTest.Tests;
+
+
+[TestFixture]
+public class RebateServiceTests
+{
+    private RebateService _target;
+    private Mock<IProductDataStore> _productDataStore;
+    private Mock<IRebateDataStore> _rebateDataStore;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _productDataStore = new Mock<IProductDataStore>();
+        _rebateDataStore = new Mock<IRebateDataStore>();
+        _target = new RebateService(_productDataStore.Object, _rebateDataStore.Object);
+    }
+
+    #region FixedRateRebate Tests
+
+    [Test]
+    public void RunRebateService_Should_Calculate_Fixed_Rate_Rebate_Success()
+    {
+        var testRequest = new CalculateRebateRequest()
+        {
+            ProductIdentifier = "PROD_ID_3E783",
+            RebateIdentifier = "REB_ID_34KD94",
+            Volume = 3.159m
+        };
+
+        var rebateResponse = new Rebate()
+        {
+            Identifier = testRequest.RebateIdentifier,
+            Amount = 100,
+            Incentive = IncentiveType.FixedRateRebate,
+            Percentage = 17
+        };
+
+        var productResponse = new Product()
+        {
+            Identifier = testRequest.ProductIdentifier,
+            Id = 9,
+            Price = 32.50m,
+            SupportedIncentives = SupportedIncentiveType.FixedRateRebate,
+            Uom = "U_O_M"
+        };
+
+        _rebateDataStore.Setup(rds => rds.GetRebate(testRequest.RebateIdentifier))
+            .Returns(rebateResponse)
+            .Verifiable();
+
+        _productDataStore.Setup(PDS => PDS.GetProduct(testRequest.ProductIdentifier))
+            .Returns(productResponse)
+            .Verifiable();
+
+        var result = _target.Calculate(testRequest);
+        Assert.That(result.Success, Is.True);
+        _rebateDataStore.Verify();
+        _productDataStore.Verify();
+    }
+
+    [Test]
+    public void RunRebateService_Should_Calculate_Fixed_Rate_Rebate_Failure()
+    {
+        var testRequest = new CalculateRebateRequest()
+        {
+            ProductIdentifier = "PROD_ID_3E783",
+            RebateIdentifier = "REB_ID_34KD94",
+            Volume = 3.159m
+        };
+
+        var rebateResponse = new Rebate()
+        {
+            Identifier = testRequest.RebateIdentifier,
+            Amount = 0,
+            Incentive = IncentiveType.FixedRateRebate,
+            Percentage = 0
+        };
+
+        var productResponse = new Product()
+        {
+            Identifier = testRequest.ProductIdentifier,
+            Id = 9,
+            Price = 32.50m,
+            SupportedIncentives = SupportedIncentiveType.FixedRateRebate,
+            Uom = "U_O_M"
+        };
+
+        _rebateDataStore.Setup(rds => rds.GetRebate(testRequest.RebateIdentifier))
+            .Returns(rebateResponse)
+            .Verifiable();
+
+        _productDataStore.Setup(PDS => PDS.GetProduct(testRequest.ProductIdentifier))
+            .Returns(productResponse)
+            .Verifiable();
+
+        var result = _target.Calculate(testRequest);
+        Assert.That(result.Success, Is.False);
+        _rebateDataStore.Verify();
+        _productDataStore.Verify();
+    }
+
+    #endregion
+
+
+    #region FixedCashRebate Tests
+
+    [Test]
+    public void RunRebateService_Should_Calculate_Fixed_Cash_Rebate_Success()
+    {
+        var testRequest = new CalculateRebateRequest()
+        {
+            ProductIdentifier = "PROD_ID_3E783",
+            RebateIdentifier = "REB_ID_34KD94",
+            Volume = 3.159m
+        };
+
+        var rebateResponse = new Rebate()
+        {
+            Identifier = testRequest.RebateIdentifier,
+            Amount = 100,
+            Incentive = IncentiveType.FixedCashAmount,
+            Percentage = 17
+        };
+
+        var productResponse = new Product()
+        {
+            Identifier = testRequest.ProductIdentifier,
+            Id = 9,
+            Price = 32.50m,
+            SupportedIncentives = SupportedIncentiveType.FixedCashAmount,
+            Uom = "U_O_M"
+        };
+
+        _rebateDataStore.Setup(rds => rds.GetRebate(testRequest.RebateIdentifier))
+            .Returns(rebateResponse)
+            .Verifiable();
+
+        _productDataStore.Setup(PDS => PDS.GetProduct(testRequest.ProductIdentifier))
+            .Returns(productResponse)
+            .Verifiable();
+
+
+        var result = _target.Calculate(testRequest);
+        Assert.That(result.Success, Is.True);
+        _rebateDataStore.Verify();
+        _productDataStore.Verify();
+    }
+
+    [Test]
+    public void RunRebateService_Should_Calculate_Fixed_Cash_Rebate_Failure()
+    {
+        var testRequest = new CalculateRebateRequest()
+        {
+            ProductIdentifier = "PROD_ID_3E783",
+            RebateIdentifier = "REB_ID_34KD94",
+            Volume = 3.159m
+        };
+
+        var rebateResponse = new Rebate()
+        {
+            Identifier = testRequest.RebateIdentifier,
+            Amount = 0,
+            Incentive = IncentiveType.FixedCashAmount,
+            Percentage = 0
+        };
+
+        var productResponse = new Product()
+        {
+            Identifier = testRequest.ProductIdentifier,
+            Id = 9,
+            Price = 32.50m,
+            SupportedIncentives = SupportedIncentiveType.FixedCashAmount,
+            Uom = "U_O_M"
+        };
+
+        _rebateDataStore.Setup(rds => rds.GetRebate(testRequest.RebateIdentifier))
+            .Returns(rebateResponse)
+            .Verifiable();
+
+        _productDataStore.Setup(PDS => PDS.GetProduct(testRequest.ProductIdentifier))
+            .Returns(productResponse)
+            .Verifiable();
+
+
+        var result = _target.Calculate(testRequest);
+        Assert.That(result.Success, Is.False);
+        _rebateDataStore.Verify();
+        _productDataStore.Verify();
+    }
+    #endregion
+
+    #region AmountPerUom Tests
+
+    [Test]
+    public void RunRebateService_Should_Calculate_Amount_Per_Uom_Rebate_Success()
+    {
+        var testRequest = new CalculateRebateRequest()
+        {
+            ProductIdentifier = "PROD_ID_3E783",
+            RebateIdentifier = "REB_ID_34KD94",
+            Volume = 3.159m
+        };
+
+        var rebateResponse = new Rebate()
+        {
+            Identifier = testRequest.RebateIdentifier,
+            Amount = 100,
+            Incentive = IncentiveType.AmountPerUom,
+            Percentage = 17
+        };
+
+        var productResponse = new Product()
+        {
+            Identifier = testRequest.ProductIdentifier,
+            Id = 9,
+            Price = 32.50m,
+            SupportedIncentives = SupportedIncentiveType.AmountPerUom,
+            Uom = "U_O_M"
+        };
+
+        _rebateDataStore.Setup(rds => rds.GetRebate(testRequest.RebateIdentifier))
+            .Returns(rebateResponse)
+            .Verifiable();
+
+        _productDataStore.Setup(PDS => PDS.GetProduct(testRequest.ProductIdentifier))
+            .Returns(productResponse)
+            .Verifiable();
+
+
+        var result = _target.Calculate(testRequest);
+        Assert.That(result.Success, Is.True);
+        _rebateDataStore.Verify();
+        _productDataStore.Verify();
+    }
+
+    [Test]
+    public void RunRebateService_Should_Calculate_Amount_Per_Uom_Rebate_Failure()
+    {
+        var testRequest = new CalculateRebateRequest()
+        {
+            ProductIdentifier = "PROD_ID_3E783",
+            RebateIdentifier = "REB_ID_34KD94",
+            Volume = 0m
+        };
+
+        var rebateResponse = new Rebate()
+        {
+            Identifier = testRequest.RebateIdentifier,
+            Amount = 0,
+            Incentive = IncentiveType.AmountPerUom,
+            Percentage = 17
+        };
+
+        var productResponse = new Product()
+        {
+            Identifier = testRequest.ProductIdentifier,
+            Id = 9,
+            Price = 32.50m,
+            SupportedIncentives = SupportedIncentiveType.AmountPerUom,
+            Uom = "U_O_M"
+        };
+
+        _rebateDataStore.Setup(rds => rds.GetRebate(testRequest.RebateIdentifier))
+            .Returns(rebateResponse)
+            .Verifiable();
+
+        _productDataStore.Setup(PDS => PDS.GetProduct(testRequest.ProductIdentifier))
+            .Returns(productResponse)
+            .Verifiable();
+
+
+        var result = _target.Calculate(testRequest);
+        Assert.That(result.Success, Is.False);
+        _rebateDataStore.Verify();
+        _productDataStore.Verify();
+    }
+    #endregion
+}
